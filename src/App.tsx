e16e24f57309dca80bbad61d712375dd92d7762a
fathia dzafira <fathiadzafira610@gmail.com>
2026-03-19 07:07:52 +0700

    update portofolio terbaru

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import HeroSection from "../src/components/HeroSection";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+          <Navbar isDark={false} toggleTheme={() => {}} />
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+      <HeroSection />
+      <Footer />
+      
+    </>
+  );
+}
 
 export default App;
